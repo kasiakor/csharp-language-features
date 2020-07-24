c28437e69c07a35d7e119ff6abf68af9a9a639e8
kasia.kordys <kasia.kordys@gmail.com>
2020-07-24 20:58:37 +0100

    Add project files.

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using LanguageFeatures.Models;
+using System;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            //create a new product object
+            Product myProduct = new Product();
+            //set the property
+            myProduct.Name = "Kayak";
+            // get the property
+            string productName = myProduct.Name;
+            //generate the view
+            return View("Result", (object)String.Format("Product name: {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            //object initializer used to create and populate a new object
+            Product myProduct = new Product
+            {
+                ProductId = 102,
+                Name = "Kayak",
+                Description = "Kayak for 1 person",
+                Price = 25,
+                Category="Infalatable"
+            };
+            return View("Result", (object)String.Format("The category of  {0} is {1}", myProduct.Name, myProduct.Category));
+        }
+    }
+}
