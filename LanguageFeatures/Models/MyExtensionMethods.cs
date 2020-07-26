0393fd1543ef0352734f791bef97392941738e10
kasia.kordys <kasia.kordys@gmail.com>
2020-07-26 19:09:41 +0100

    add extension method

---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        //this extension method
+        //cartParam instance of the Shopping cart
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            foreach (Product product  in cartParam.Products)
+            {
+                total += product.Price;
+            }
+            return total;
+        }
+    }
+}
